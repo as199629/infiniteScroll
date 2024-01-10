1f4c1b5ebc00ba7254452452ec5a094634971908
ericyang <ericyang@gis.tw>
2024-01-10 15:51:42 +0800

    eslint規範修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,31 @@
 module.exports = {
-    extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-    parserOptions: {
-        parser: "babel-eslint"
-    }
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020, // 设置支持的 ECMAScript 版本
+    sourceType: 'module', // 启用 ES6 模块语法
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-env'],
+    },
+  },
+  plugins: ['import'],
+
+  rules: {
+    'import/no-unresolved': 'off',
+    // ...其他规则设置
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+      },
+    ],
+  },
 };

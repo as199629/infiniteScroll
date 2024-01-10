4965ee4a5280cd70f184051bfa4ed0dabf91d4ec
ericyang <ericyang@gis.tw>
2024-01-10 14:33:15 +0800

    1.github api串接 2.新增滾輪監聽事件 3.設定每次只讀取10筆資料

--- src/components/InfiniteScroll.vue
+++ src/components/InfiniteScroll.vue
@@ -14,54 +14,54 @@
 </template>
 
 <script setup>
-  import { ref, onMounted, onUnmounted, getCurrentInstance } from "vue";
-  const instance = getCurrentInstance();
-  const users = ref([]); // 存放所有的使用者資料
-  const displayedUsers = ref([]); // 顯示的使用者資料
-  const usersPerPage = 10; // 每次載入的使用者數
-  const currentPage = ref(1); // 目前頁碼
-  const loading = ref(false); // 加載狀態標誌位
+import { ref, onMounted, onUnmounted, getCurrentInstance } from 'vue'
+const instance = getCurrentInstance()
+const users = ref([]) // 存放所有的使用者資料
+const displayedUsers = ref([]) // 顯示的使用者資料
+const usersPerPage = 10 // 每次載入的使用者數
+const currentPage = ref(1) // 目前頁碼
+const loading = ref(false) // 加載狀態標誌位
 
-  const fetchUsers = async () => {
-    if (loading.value) return; // 如果正在加載，則不執行
-    loading.value = true; // 設置為正在加載
-    try {
-      const response = await fetch(
-        `https://api.github.com/users?per_page=${usersPerPage}&since=${currentPage.value}`
-      );
-      if (!response.ok) {
-        throw new Error("Failed to fetch data");
-      }
-      const data = await response.json();
-      users.value = [...users.value, ...data];
-      currentPage.value++;
-    } catch (error) {
-      console.error(error);
-    } finally {
-      loading.value = false; // 加載完成後重置為false
+const fetchUsers = async () => {
+  if (loading.value) return // 如果正在加載，則不執行
+  loading.value = true // 設置為正在加載
+  try {
+    const response = await fetch(
+      `https://api.github.com/users?per_page=${usersPerPage}&since=${currentPage.value}`,
+    )
+    if (!response.ok) {
+      throw new Error('Failed to fetch data')
     }
-  };
+    const data = await response.json()
+    users.value = [...users.value, ...data]
+    currentPage.value++
+  } catch (error) {
+    console.error(error)
+  } finally {
+    loading.value = false // 加載完成後重置為false
+  }
+}
 
-  const handleScroll = () => {
-    const observer = instance.refs.scrollObserver;
-    const scrollPos = window.scrollY + window.innerHeight;
-    const observerTop = observer.offsetTop;
-    const observerHeight = observer.offsetHeight;
+const handleScroll = () => {
+  const observer = instance.refs.scrollObserver
+  const scrollPos = window.scrollY + window.innerHeight
+  const observerTop = observer.offsetTop
+  const observerHeight = observer.offsetHeight
 
-    if (scrollPos >= observerTop + observerHeight * 0.9) {
-      if (displayedUsers.value.length < users.value.length) {
-        fetchUsers();
-      }
+  if (scrollPos >= observerTop + observerHeight * 0.9) {
+    if (displayedUsers.value.length < users.value.length) {
+      fetchUsers()
     }
-  };
+  }
+}
 
-  onMounted(() => {
-    fetchUsers();
+onMounted(() => {
+  fetchUsers()
 
-    window.addEventListener("scroll", handleScroll);
-  });
+  window.addEventListener('scroll', handleScroll)
+})
 
-  onUnmounted(() => {
-    window.removeEventListener("scroll", handleScroll);
-  });
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
 </script>

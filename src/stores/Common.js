a6670647501651f6e21ff1316531b0802e3ebd17
ericyang <ericyang@gis.tw>
2024-01-10 14:32:17 +0800

    刪除多餘程式碼

--- src/stores/Common.js
+++
@@ -1,8 +0,0 @@
-import { ref } from 'vue'
-import { defineStore } from 'pinia'
-import axios from 'axios'
-
-export const useApiCommonStore = defineStore('apiCommon', () => {
-
-  return {};
-})
--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,7 @@ export default defineConfig({
   plugins: [vue()],
   base: "./",
   server: {
+    host: 'localhost',
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:49501/',
-        // Referer: 'http://localhost:8888/api',
-        changeOrigin: true,
-        secure: false,
-        headers: {
-          target: 'http://localhost:49501/',
-          // Referer: 'http://localhost:8888/api',
-        },
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-    },
   },
 })
